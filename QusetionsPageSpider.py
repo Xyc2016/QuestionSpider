6b1823581a6b8bfe28c22535989383b4aad9f3d0
X <986327386@qq.com>
2017-01-12 16:22:17 +0800

    16:22

--- QusetionsPageSpider.py
+++ QusetionsPageSpider.py
@@ -2,13 +2,29 @@ from urllib import request
 from bs4 import BeautifulSoup
 import re
 link='https://www.zhihu.com/question/38478558'
-def f1(link):
+def realTitle(title):
+    s=''
+    for c in title:
+        if c!='\n':
+            s+=c
+    return s
+
+def PageSearch(link, topic=None):
     webPage=request.urlopen(link).read()
     soup=BeautifulSoup(webPage,'html5lib')
-    # print(soup.title.string)
-    fileName='newFolder/'+link[-8:]+'.txt'
+    topicidSet=set()
+
+    if topic!=None:
+        for A in soup.find_all('a'):
+            if A.get('data-topicid'):
+                topicidSet.add(A.get('data-topicid'))
+        if topic not in topicidSet:
+            return []
+
+    fileName='linkFolder/'+link[-8:]+'.desktop'
     file=open(fileName,'w')
-    file.write(soup.title.string)
+    text = '[Desktop Entry]\nEncoding=UTF-8\nName=' + realTitle(soup.title.string) + '\nType=Link\nURL=https://www.zhihu.com/question/'+link[-8:]+'\nIcon=text-html'
+    file.write(text)
     search=re.compile(r'(?<=question/).*?(?=" data-id=)')
     newLinks=[]
     for item in search.findall(str(webPage)):
@@ -25,10 +41,11 @@ while True:
     for item in Links:
         if item not in alreadyDownload:
             try:
-                newLinks+=f1(item)
+                newLinks+=PageSearch(item)
                 alreadyDownload.add(item)
             except Exception as e:
                 continue
 
     Links=newLinks
     print(Links)
+
